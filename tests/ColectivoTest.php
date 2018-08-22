89ebef787e2e5ae328e21b0956e7e360f0f9350d
Mauro Tebay <39992519+maurotebay@users.noreply.github.com>
2018-08-22 19:15:12 -0300

    Comentado testViajesPlus() y testPagarSaldoSuf()

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -22,18 +22,20 @@ class ColectivoTest extends TestCase {
         $tarjeta = new Tarjeta;
         $boleto = new Boleto(14.80, $colectivo, $tarjeta);
         $tarjeta->recargar(20);
+        
+        //testeamos si al pagar con la tarjeta con saldo suficiente se emite un boleto correcto
         $this->assertEquals($colectivo->pagarCon($tarjeta),$boleto);
     }
 
     public function testViajesPlus() {
         $colectivo = new Colectivo;
         $tarjeta = new Tarjeta;
-        $plus1 = new Boleto("Viaje Plus", $colectivo, $tarjeta);
-        $plus2 = new Boleto("Ultimo Plus", $colectivo, $tarjeta);
+        $plus1 = new Boleto("Viaje Plus", $colectivo, $tarjeta);    //primero creamos dos boletos, uno siendo un plus normal o primer plus
+        $plus2 = new Boleto("Ultimo Plus", $colectivo, $tarjeta);   //y el otro es correspondiente a un segundo o ultimo plus
         
-        $tarjeta->recargar(10);
-        $this->assertEquals($colectivo->pagarCon($tarjeta),$plus1);
-        $this->assertEquals($colectivo->pagarCon($tarjeta),$plus2);
+        $tarjeta->recargar(10);         //recargamos una cantidad insuficiente de dinero en la tarjeta para que esta utilice los viajes plus
+        $this->assertEquals($colectivo->pagarCon($tarjeta),$plus1);     //primero testeamos si se emite correctamente el primer plus
+        $this->assertEquals($colectivo->pagarCon($tarjeta),$plus2);     //y luego si se emite correctamente el boleto del ultimo plus
 
     }
 
